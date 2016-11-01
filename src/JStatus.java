79e3356c0168dec09ab56c7d5963243d1f8a7dcb
eczn <eczn@moebaka.com>
2016-11-01 22:57:21 +0800

    to record JPlayer's status

--- src/JStatus.java
+++ src/JStatus.java
@@ -1,13 +1,17 @@
+import javafx.scene.media.MediaPlayer;
+
 public class JStatus {
     // 是否在播放
     public boolean isPlay;
-
+    public MediaPlayer nowPlay;
     //
 //    private boolean
 
     public JStatus(){
         isPlay = false;
+        nowPlay = null;
     }
 
-//    public set
 }
+
+
