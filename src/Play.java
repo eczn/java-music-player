b74d0e69ed3d41dada99dc1d3745664e2b2ccea1
eczn <eczn@moebaka.com>
2016-11-01 22:57:31 +0800

    cut it!

--- src/Play.java
+++ src/Play.java
@@ -1,28 +1,3 @@
-import java.net.URL;
+public class Play{
 
-import javafx.application.Application;
-import javafx.scene.media.Media;
-import javafx.scene.media.MediaPlayer;
-import javafx.stage.Stage;
-
-public class Play extends Application {
-
-    public static void main(String[] args) {
-        launch(args);
-//        new Play();
-    }
-
-    @Override
-    public void start(Stage primaryStage) {
-        final URL resource = getClass().getResource("a.mp3");
-
-        final Media media = new Media(resource.toString());
-        final MediaPlayer mediaPlayer = new MediaPlayer(media);
-        mediaPlayer.play();
-
-        primaryStage.setTitle("Audio Player 1");
-        primaryStage.setWidth(200);
-        primaryStage.setHeight(200);
-        primaryStage.show();
-    }
 }
